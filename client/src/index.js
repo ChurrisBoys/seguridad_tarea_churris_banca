c45547da27ded7d4a733feb1c7771aefec8742a4
EMILIA MARIA VIQUEZ MORA <emilia.viquez@ucr.ac.cr>
2024-04-19 22:10:03 -0600

    Added views to the index.js file so that we can see the windows while creating them

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import BankingFeed from './components/Banking/BankingFeed';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Using Router to define paths to different components
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/banking" element={<BankingFeed />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
