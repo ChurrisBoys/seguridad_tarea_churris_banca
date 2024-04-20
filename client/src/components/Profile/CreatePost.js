09863022607efdbc38725da6c97531d4b8781e5d
EMILIA MARIA VIQUEZ MORA <emilia.viquez@ucr.ac.cr>
2024-04-20 00:34:48 -0600

    Added the small icon to the app view, added camera icon to the create post page and redirected the Exit button in the create post page to the login page

--- client/src/components/Profile/CreatePost.js
+++ client/src/components/Profile/CreatePost.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import camera from './camera.png';
 import './CreatePost.css';
 
 function CreatePost() {
+  const navigate = useNavigate(); 
+
+  const handleExit = () => {
+    navigate('/'); 
+  };
+
   return (
     <div className="CreatePost">
       <div className="top-buttons">
@@ -11,14 +19,22 @@ function CreatePost() {
         </div>
         <div className="buttons-right">
           <button className="button">My Profile</button>
-          <button className="button">Exit</button>
+          <button className="button" onClick={handleExit}>Exit</button>        
         </div>
       </div>
       <div className="content">
         <h2 className="title">Create Post</h2>
         <button className="App-Submit-Button">Publish</button>
       </div>
-      <div className="gray-rectangle"></div>
+      <div className="gray-rectangle">
+        <div className="upload-and-comment">
+          <label htmlFor="file-input" className="upload-icon-container">
+          <img src={camera} alt="Upload Icon" className="upload-icon" />
+          </label>
+          <textarea className="post-text" placeholder="Write a comment..."></textarea>
+        </div>
+        <input id="file-input" type="file" style={{ display: 'none' }} />
+      </div>
     </div>
   );
 }
