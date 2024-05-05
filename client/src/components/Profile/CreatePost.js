6d640e37630b9537184977378853b1235b1b1450
Carlos Antonio <CARLOS.sanchezblanco@ucr.ac.cr>
2024-05-05 13:40:56 -0600

    Modified creaetePost to send a POST request with image and description of the Post in its body to the server

--- client/src/components/Profile/CreatePost.js
+++ client/src/components/Profile/CreatePost.js
@@ -4,14 +4,14 @@ import camera from './camera.png';
 import './CreatePost.css';
 
 function CreatePost() {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const handleExit = () => {
-    navigate('/'); 
+    navigate('/');
   };
 
   const handleTransaction = () => {
-    navigate('/banking'); 
+    navigate('/banking');
   };
 
   return (
@@ -23,22 +23,29 @@ function CreatePost() {
         </div>
         <div className="buttons-right">
           <button className="button">My Profile</button>
-          <button className="button" onClick={handleExit}>Exit</button>        
+          <button className="button" onClick={handleExit}>Exit</button>
         </div>
       </div>
-      <div className="content">
-        <h2 className="title">Create Post</h2>
-        <button className="App-Submit-Button">Publish</button>
-      </div>
-      <div className="gray-rectangle">
-        <div className="upload-and-comment">
-          <label htmlFor="file-input" className="upload-icon-container">
-          <img src={camera} alt="Upload Icon" className="upload-icon" />
-          </label>
-          <textarea className="post-text" placeholder="Write a comment..."></textarea>
+      <form
+        action="http://localhost:3001/api/createpost"
+        method="post"
+        encType="multipart/form-data"
+        className="form-content"
+      >
+        <div className="content">
+          <h2 className="title">Create Post</h2>
+          <button className="App-Submit-Button">Publish</button>
         </div>
-        <input id="file-input" type="file" style={{ display: 'none' }} />
-      </div>
+        <div className="gray-rectangle">
+          <div className="upload-and-comment">
+            <label for="image" className="upload-icon-container">
+              <img src={camera} alt="Upload Icon" className="upload-icon" />
+            </label>
+            <textarea id="post-text-id" name="user-description" className="post-text" placeholder="Write a comment..."></textarea>
+          </div>
+          <input name="user-image" id="image" type="file" accept="image/*" style={{ display: 'none' }} />
+        </div>
+      </form>
     </div>
   );
 }
