bb83cc6fb5804ae167c9465c669c1dab7692e06c
alr201 <e.alonsoleon@gmail.com>
2024-05-23 02:04:28 -0600

    Added the edit function and deleted the userIcon

--- client/src/components/Profile/MyProfile.js
+++ client/src/components/Profile/MyProfile.js
@@ -1,29 +1,39 @@
-import React from 'react';
+import React , { useState, useEffect }from 'react';
 import { useNavigate } from "react-router-dom"
 import ImgAsset from '../Social/public'
-import userIcon from './UserIcon.svg'
 import Layout from "../Common/Layout";
 import './MyProfile.css';
 import DisplayPosts from '../Social/DisplayPosts';
-
+import EditProfilePopup from './EditProfilePopup';
 
 
 function MyProfile() {
-
     const navigate = useNavigate();
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const username = 'Emilia'; // TODO: Change to actual user
 
     const handleCreatePost = () => {
         navigate('/createpost');
     };
 
+    const handleEditProfile = () => {
+        setIsPopupOpen(true);
+    };
+
+    const closePopup = () => {
+        setIsPopupOpen(false);
+    };
+
     return (
         <Layout>
-            <div className="MyProfile">
+            <div className="MyProfile"> 
                 <div className="user-Info">
-                    <img src={userIcon} alt='User Icon' className='user-icon' />
                     <div className='user-text'>
-                        <h2 className="title">My Name</h2>
-                        <h4>My Info</h4>
+                        <h1 className="title">My Name</h1>
+                        <h3>My Info</h3>
+                    </div>
+                    <div className='modify-personal-info-button'>
+                        <button className="App-Submit-Button" onClick={handleEditProfile}>Edit</button>
                     </div>
                     <div className='create-post-button'>
                         <button className="App-Submit-Button" onClick={handleCreatePost}>Create Post</button>
@@ -35,6 +45,7 @@ function MyProfile() {
         			<DisplayPosts itemsOnPage={3}/>
                 </div>
             </div>
+            {isPopupOpen && <EditProfilePopup username={username} onClose={closePopup} />}
         </Layout>
     );
 }
