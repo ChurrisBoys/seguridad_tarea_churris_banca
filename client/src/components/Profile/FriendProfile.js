6f1466ef43a3943409039ed7f4e4dc72b4fa1d2a
EMILIA MARIA VIQUEZ MORA <emilia.viquez@ucr.ac.cr>
2024-05-25 15:50:42 -0600

    Added user email and phone number to the friend profile

--- client/src/components/Profile/FriendProfile.js
+++ client/src/components/Profile/FriendProfile.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom'; 
 import Layout from "../Common/Layout";
 import './MyProfile.css';
+import config from "../../config";
 import DisplayPosts from '../Social/DisplayPostsUser';
 
 function FriendProfile() {
-    // const navigate = useNavigate();
 
     const { username } = useParams();  // Extract the username
+
+    const [userInfo, setUserInfo] = useState(null);
+
+    useEffect(() => {
+        fetch(`${config.BASE_URL}/api/profile/${username}`)
+            .then(response => response.json())
+            .then(data => setUserInfo(data))
+            .catch(error => console.error('Error fetching user data:', error));
+    }, [username]);
 
     return (
         <Layout>
@@ -16,6 +25,14 @@ function FriendProfile() {
                     <div className='user-text'>
                         <h2 className="title">{username}</h2>
                         <h4>Information about {username}</h4>
+                        {userInfo ? (
+                            <div>
+                                <p>Phone number: {userInfo.telnum}</p>
+                                <p>Email: {userInfo.email}</p>
+                            </div>
+                        ) : (
+                            <p>Loading user information...</p>
+                        )}
                     </div>
                 </div>
 
