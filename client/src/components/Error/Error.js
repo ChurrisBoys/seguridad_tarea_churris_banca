7af0faa8a26c69ef0986452bca11d4e02e4817af
EMILIA MARIA VIQUEZ MORA <emilia.viquez@ucr.ac.cr>
2024-05-26 21:48:31 -0600

    Error page image

--- client/src/components/Error/Error.js
+++ client/src/components/Error/Error.js
@@ -1,16 +1,11 @@
 import React from 'react';
+import errorImage from '../../error_page.png'; 
 
 function Error() {
   return (
-    <div style={{ height: '100vh', margin: '5%' }}>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '15%' }}>
-        <h1 style={{ color: 'black' }}>No, no, no</h1>
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '15%' }}>
-        <h1 style={{ color: 'black' }}>Eso no se puede jijiji</h1>
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '15%' }}>
-        <h1 style={{ color: 'black' }}>Hay que portarse bien</h1>
+    <div style={{ height: '100vh', margin: '2%' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+        <img src={errorImage} alt="Error" style={{ maxWidth: '100%', maxHeight: '100%' }} />
       </div>
     </div>
   );
